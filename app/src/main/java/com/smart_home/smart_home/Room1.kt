8c7e528113b79c5007224f4d3e498b16e83de036
rana ahmed <rana.ahmed99000@gmail.com>
2019-04-26 03:13:32 +0200

    added AC and curtains features in room1 and made some chnages in main activity

--- app/src/main/java/com/smart_home/smart_home/Room1.kt
+++ app/src/main/java/com/smart_home/smart_home/Room1.kt
@@ -2,6 +2,7 @@ package com.smart_home.smart_home
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ToggleButton
 import android.widget.Toast
 import com.google.firebase.database.FirebaseDatabase
@@ -12,11 +13,86 @@ import com.google.firebase.database.ValueEventListener
 
 class Room1 : AppCompatActivity() {
 
-    var roomsDatabase = FirebaseDatabase.getInstance().getReference("Rooms")
-    var light = roomsDatabase.child("Room1").child("Light")
+    private var roomsDatabase = FirebaseDatabase.getInstance().getReference("Rooms")
+    private var light = roomsDatabase.child("Room1").child("Light")
+    private var curtains = roomsDatabase.child("Room1").child("Curtains")
+    private var AC = roomsDatabase.child("Room1").child("AC")
+    private var temperature = roomsDatabase.child("Room1").child("Temperature")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_room1)
+        controlLight()
+        controlCurtains()
+        controlAC()
+        controlTemperature()
+    }
+
+    private fun controlTemperature(){
+        val temp1ET = findViewById<EditText>(R.id.temperature1ET)
+        temperature.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                //val value = dataSnapshot.getValue(Int::class.java)
+                //temp1ET.setText(value!!)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@Room1, "failed", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+    }
+    private fun controlAC(){
+        val AC1TB = findViewById<ToggleButton>(R.id.AC1TB)
+        AC1TB?.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked)
+                curtains.setValue("ON")
+            else curtains.setValue("OFF")
+        }
+        AC.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val value = dataSnapshot.getValue(String::class.java)
+                if (value == "ON") {
+                    AC1TB.setTextOn("ON")
+                    AC1TB.setChecked(true)
+                } else {
+                    AC1TB.setTextOff("OFF")
+                    AC1TB.setChecked(false)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@Room1, "failed", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+    }
+
+    private fun controlCurtains(){
+        val curtains1TB = findViewById<ToggleButton>(R.id.curtains1TB)
+        curtains1TB?.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked)
+                curtains.setValue("Opened")
+            else curtains.setValue("Closed")
+        }
+        curtains.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val value = dataSnapshot.getValue(String::class.java)
+                if (value == "Opened") {
+                    curtains1TB.setTextOn("Opened")
+                    curtains1TB.setChecked(true)
+                } else {
+                    curtains1TB.setTextOff("Closed")
+                    curtains1TB.setChecked(false)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@Room1, "failed", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun controlLight(){
         val light1TB = findViewById<ToggleButton>(R.id.light1TB)
         light1TB?.setOnCheckedChangeListener { buttonView, isChecked ->
             val msg = "Toggle Button is " + if (isChecked) "ON" else "OFF"
@@ -26,12 +102,8 @@ class Room1 : AppCompatActivity() {
             else light.setValue("OFF")
         }
 
-
-
         light.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
                 val value = dataSnapshot.getValue(String::class.java)
                 if (value == "ON") {
                     light1TB.setTextOn("ON")
@@ -40,16 +112,12 @@ class Room1 : AppCompatActivity() {
                     light1TB.setTextOff("OFF")
                     light1TB.setChecked(false)
                 }
-
             }
 
             override fun onCancelled(error: DatabaseError) {
-                // Failed to read value
                 Toast.makeText(this@Room1, "failed", Toast.LENGTH_SHORT).show()
-
             }
         })
-
     }
 
 
